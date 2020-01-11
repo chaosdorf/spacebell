7546f6668043e23c9226d194837e803998cc65e0
A2nkF <a2nkf.a2nkf@gmail.com>
2020-01-11 19:43:54 +0100

    Add rate limiting

--- spacebell.py
+++ spacebell.py
@@ -2,17 +2,31 @@ import argparse
 import paho.mqtt.client as mqtt
 import pulsectl
 
+import threading
+import time
+
+BELL = False
 
 def on_connect(mqtt_server, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
     mqtt_server.subscribe(args.topic)
 
-
 def on_message(mqtt_server, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    print(args.sample)
-    pulse_server.play_sample(args.sample, pulse_sinks[0])
+    global BELL
+    BELL = True
+    bell_player()
 
+    print(msg.topic + " " + str(msg.payload))
+    print(args.sample)
+
+def bell_player():
+    global BELL
+
+    while not BELL:
+            print("**BELL**")
+            pulse_server.play_sample(args.sample, pulse_sinks[0])
+            time.sleep(10)
+            BELL = False
 
 parser = argparse.ArgumentParser(description='The Chaosdorf doorbell service')
 
@@ -35,5 +49,10 @@ mqtt_server.on_connect = on_connect
 mqtt_server.on_message = on_message
 
 mqtt_server.connect(args.server, 1883, 60)
+
+# Rate limiting 
+ringer_thread = threading.Thread(target=bell_player)
+ringer_thread.setDaemon(True)
+ringer_thread.start()
 
 mqtt_server.loop_forever()
